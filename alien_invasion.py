54eba323dd21b17fe6cd10490dc854919673d98a
FedeMaguire <fedemaguire@hotmail.com>
2023-02-23 00:06:55 -0300

    Ship has left\right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,7 +24,9 @@ class  AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
+            
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
@@ -34,11 +36,22 @@ class  AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RCTRL:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_RALT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RCTRL:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_RALT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+        self.ship.blitme()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.

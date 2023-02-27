f6c18be2214eccb19fbfb1b1e37ad334206b5b99
FedeMaguire <fedemaguire@hotmail.com>
2023-02-27 17:13:26 -0300

    Delet old bullets and limit number

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,8 +30,14 @@ class  AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
-            self._update_screen()
             
+            # Get rid of bullets that have dissapeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+
+            self._update_screen()
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
@@ -65,8 +71,9 @@ class  AlienInvasion:
 
     def fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
--- settings.py
+++ settings.py
@@ -12,7 +12,8 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet_settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 3.0
         self.bullet_width = 6
         self.bullet_height = 15
         self.bullet_color = (180, 30, 20)
+        self.bullets_allowed = 5

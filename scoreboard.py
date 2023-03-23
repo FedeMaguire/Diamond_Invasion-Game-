55016b2de3df1f0608d7efc862ea022e67b3c148
FedeMaguire <fedemaguire@hotmail.com>
2023-03-23 18:36:52 -0300

    Added level info,  score labels

--- scoreboard.py
+++ scoreboard.py
@@ -11,17 +11,18 @@ class Scoreboard:
         self.stats = ai_game.stats
 
         # Font settings for scoring information
-        self.text_color = (255, 255, 255)
-        self.font = pygame.font.Font("gameFont.ttf", 36)
+        self.text_color = (235, 143, 255)
+        self.font = pygame.font.Font("gameFont.ttf", 24)
 
         # Prepare the initial score images.
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Turn the score into a rendered image"""
         rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
+        score_str = "{:,}".format(rounded_score) + " SCORE"
         self.score_image = self.font.render(score_str, True,
                 self.text_color)
         
@@ -31,14 +32,15 @@ class Scoreboard:
         self.score_rect.top = 20
 
     def show_score(self):
-        """Draw score to the screen."""
+        """Draw score and level to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "{:,}".format(high_score) + " HIGH SCORE"
         self.high_score_image = self.font.render(high_score_str, True,
                 self.text_color)
         
@@ -52,3 +54,14 @@ class Scoreboard:
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
+
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        level_str = str(self.stats.level) + " LEVEL"
+        self.level_image = self.font.render(level_str, True,
+                self.text_color)
+        
+        # Position the level below the score.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
